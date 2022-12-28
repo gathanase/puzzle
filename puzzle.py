e1d06982cde7db7b950dc5c40ba7651063292036
Gregoire Athanase <gathanase@antidot.net>
2022-12-28 23:27:34 +0100

    More robust processing

--- puzzle.py
+++ puzzle.py
@@ -39,6 +39,10 @@ class Piece():
     def __init__(self, idx, img_orig, img_gray, img_edges, contour):
         self.idx = idx
         x, y, w, h = cv2.boundingRect(contour)
+        x -= 1
+        y -= 1
+        w += 2
+        h += 2
         self.img_orig = img_orig[y:y+h, x:x+w]
         self.img_gray = img_gray[y:y+h, x:x+w]
         self.img_edges = img_edges[y:y+h, x:x+w]
@@ -49,8 +53,8 @@ class Piece():
         self.rotated_rect = cv2.minAreaRect(self.contour)
         self.rotated_box = np.int0(cv2.boxPoints(self.rotated_rect))
         (cx, cy), (sx, sy), angle = self.rotated_rect
-        self.area = np.sum(self.img_gray > 0.6)
-        self.lines = cv2.HoughLines(self.img_edges, 1, np.pi / 180, 10, None, 0, 0)
+        self.area = cv2.contourArea(self.contour)
+        self.lines = cv2.HoughLines(self.img_edges, 2, np.pi / 180, 30, None, 0, 0)
         if self.lines is None:
             self.lines = []
 
@@ -59,13 +63,14 @@ class Piece():
         img_orig = cv2.copyMakeBorder(self.img_orig, 20, 20, 20, 20, cv2.BORDER_CONSTANT)
         (cx, cy), (sx, sy), angle = self.rotated_rect
         matrix = cv2.getRotationMatrix2D((20 + cx, 20 + cy), angle, 1.0)
-        h, w, _ = img_orig.shape
+        h, w = img_orig.shape[:2]
         cv2.warpAffine(img_orig, matrix, (w, h), img_orig)
         if sx > sy:
             img_orig = cv2.rotate(img_orig, cv2.ROTATE_90_CLOCKWISE)
         img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+        _, img_binary = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
         img_edges = cv2.Canny(image=img_gray, threshold1=100, threshold2=200)
-        (contours, _) = cv2.findContours(img_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        (contours, _) = cv2.findContours(img_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         return Piece(self.idx, img_orig, img_gray, img_edges, contours[0])
 
 
@@ -76,13 +81,13 @@ class Piece():
         self.is_border_bottom = False
         self.is_likely_border_left = False
         self.is_likely_border_right = False
-
-        for line in self.lines[:2]:
+    
+        for line in self.lines:
             rho, thetaRadians = line[0]
             theta = math.degrees(thetaRadians)
             pVertical = bell(0.01, diff_angle((theta+90) % 180, 0), 90)
             pHorizontal = bell(0.01, diff_angle(theta % 180, 90), 0)
-
+    
             if pHorizontal * bell(100, abs(rho/self.h), 0) > 0.2:
                 self.is_border_top = True
             if pHorizontal * bell(100, abs(rho/self.h), 1) > 0.2:
@@ -144,15 +149,22 @@ class Piece():
         index_bottom_right = min([(distance(p[0], self.corner_bottom_right), idx) for idx, p in enumerate(self.contour)])[1]
 
         # we always have index_top_left < index_bottom_left < index_bottom_right < index_top_right
-        self.points_left = self.contour[index_top_left:index_bottom_left]
-        self.points_bottom = self.contour[index_bottom_left:index_bottom_right]
-        self.points_right = self.contour[index_bottom_right:index_top_right]
-        self.points_top = np.concatenate([self.contour[index_top_right:], self.contour[:index_top_left]])
+        self.points_left = self.contour[index_top_left:index_bottom_left+1]
+        self.points_bottom = self.contour[index_bottom_left:index_bottom_right+1]
+        self.points_right = self.contour[index_bottom_right:index_top_right+1]
+        self.points_top = np.concatenate([self.contour[index_top_right:], self.contour[:index_top_left+1]])
+
+    def compute_fingerprint(self, contour):
+        pass
+        # print(self.idx, contour)
+        # x0, y0 = contour[0][0]
+        # x1, y1 = contour[-1][0]
 
     def analyze(self):
         self.compute_borders()
         self.compute_corners()
         self.compute_edges()
+        # self.compute_fingerprint(self.points_left)
 
 
 class Solver():
@@ -164,8 +176,9 @@ class Solver():
         img_orig = cv2.imread(filename)
         img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
         # img_blur = cv2.blur(img_gray, (3, 3))  # Produces some wrong contours
+        _, img_binary = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
         img_edges = cv2.Canny(image=img_gray, threshold1=100, threshold2=200)
-        (contours, _) = cv2.findContours(img_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        (contours, _) = cv2.findContours(img_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         print(f"Number of detected contours: {len(contours)}")
         pieces = [Piece(idx, img_orig, img_gray, img_edges, contour) for idx, contour in enumerate(contours)]
 
@@ -194,6 +207,7 @@ class Solver():
 
 class Display():
     def show(self, pieces):
+        print("Display")
         nb_tiles = len(pieces) * 1
         nb_cols = round(math.sqrt(nb_tiles))
         nb_cols = nb_cols + nb_cols % 2
@@ -207,31 +221,32 @@ class Display():
 
     def draw(self, plt, piece):
         img = piece.img_orig
+        # img = piece.img_edges
         # (cx, cy), (sx, sy), angle = piece.rotated_rect
         # cv2.drawMarker(img, (int(cx), int(cy)), (255, 0, 0))
-        # for line in piece.lines[:1]:
+        # for line in piece.lines[:5]:
         #     rho, thetaRadians = line[0]
         #     self.draw_polar_line(img, rho, thetaRadians)
-        #cv2.drawContours(img, [piece.rotated_box], 0, (0, 255, 0), 1)
-        #cv2.drawContours(img, [piece.contour], 0, (255, 0, 0), 1)
-        #cv2.line(img, piece.corner_top_left, piece.corner_bottom_right, (0, 255, 0), 1, cv2.LINE_AA)
-        #cv2.line(img, piece.corner_top_right, piece.corner_bottom_left, (0, 255, 0), 1, cv2.LINE_AA)
+        # cv2.drawContours(img, [piece.rotated_box], 0, (0, 255, 0), 1)
+        # cv2.drawContours(img, [piece.contour], 0, (255, 0, 0), 1)
+        # cv2.line(img, piece.corner_top_left, piece.corner_bottom_right, (0, 255, 0), 1, cv2.LINE_AA)
+        # cv2.line(img, piece.corner_top_right, piece.corner_bottom_left, (0, 255, 0), 1, cv2.LINE_AA)
         for p in piece.points_top:
-            cv2.drawMarker(img, p[0], (255, 0, 0), markerSize=1)
+            cv2.drawMarker(img, p[0], (255, 0, 0), markerSize=2)
         for p in piece.points_bottom:
-            cv2.drawMarker(img, p[0], (0, 255, 0), markerSize=1)
+            cv2.drawMarker(img, p[0], (0, 255, 0), markerSize=2)
         for p in piece.points_left:
-            cv2.drawMarker(img, p[0], (255, 255, 0), markerSize=1)
+            cv2.drawMarker(img, p[0], (255, 255, 0), markerSize=2)
         for p in piece.points_right:
-            cv2.drawMarker(img, p[0], (0, 0, 255), markerSize=1)
+            cv2.drawMarker(img, p[0], (0, 0, 255), markerSize=2)
         self.draw_text(img, str(piece.idx))
         plt.imshow(img)
 
     def draw_text(self, img, text):
         font = cv2.FONT_HERSHEY_SIMPLEX
-        img_h, img_w, _ = img.shape
-        size = 1
-        weight = 2
+        img_h, img_w = img.shape[:2]
+        size = 0.5
+        weight = 1
         text_w, text_h = cv2.getTextSize(text, font, size, weight)[0]
         cv2.putText(img, text, ((img_w-text_w)//2, (img_h+text_h)//2), font, size, (255, 100, 0), weight)
 
@@ -250,7 +265,11 @@ solver.read_pieces('jigsawsqr.png')
 solver.rotate_pieces()
 solver.analyze_pieces()
 
+# pieces = solver.pieces
 pieces = [piece for piece in solver.pieces if piece.is_border_top or piece.is_border_bottom]
+# pieces = [piece for piece in solver.pieces if not (piece.is_border_top or piece.is_border_bottom)]
+# pieces = [piece for piece in solver.pieces if piece.idx in [11, 41]]
 
+page = 1
 display = Display()
-display.show(pieces)
+display.show(pieces[(page - 1)*48:page*48])
